8819e93973dd6166ccd31abe29cd4a62bb76e3c1
Alexander Grube <alexp.grube@googlemail.com>
2023-04-15 12:27:54 +0200

    final stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,153 @@
-import { Button } from "@mui/material";
-import "./App.css";
+import React, { useMemo, useState } from 'react';
+import MaterialReactTable, {
+  type MRT_ColumnDef,
+  type MRT_ColumnFiltersState,
+  type MRT_PaginationState,
+  type MRT_SortingState,
+} from 'material-react-table';
+import { IconButton, Tooltip } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
 
-import MUIDataTable, { MUIDataTableColumnDef, MUIDataTableOptions } from "mui-datatables";
-
-const columns: MUIDataTableColumnDef[] = [
-  {
-    name: "Name",
-    options: {
-      filter: true,
-    },
-  },
-  {
-    name: "Company",
-    options: {
-      filter: false,
-    },
-  },
-  {
-    name: "City",
-    options: {
-      filter: true,
-    },
-  },
-  {
-    name: "State",
-    options: {
-      filter: true,
-    },
-  },
-];
-
-const data = [
-  ["Joe James", "Test Corp", "Yonkers", "NY"],
-  ["John Walsh", "Test Corp", "Hartford", "CT"],
-  ["Bob Herm", "Test Corp", "Tampa", "FL"],
-  ["James Houston", "Test Corp", "Dallas", "TX"],
-];
-
-const options: MUIDataTableOptions = {
-  filterType: "checkbox",
+type UserApiResponse = {
+  data: Array<User>;
+  meta: {
+    totalRowCount: number;
+  };
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Button variant="contained" color="primary">
-        BRUHHH
-      </Button>
-      <MUIDataTable
-        title={"Employee List"}
-        data={data}
-        columns={columns}
-        options={options}
-      />
-    </div>
-  );
-}
+type User = {
+  firstName: string;
+  lastName: string;
+  address: string;
+  state: string;
+  phoneNumber: string;
+};
 
-export default App;
+const Example = () => {
+  const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>(
+    [],
+  );
+  const [globalFilter, setGlobalFilter] = useState('');
+  const [sorting, setSorting] = useState<MRT_SortingState>([]);
+  const [pagination, setPagination] = useState<MRT_PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  const { data, isError, isFetching, isLoading, refetch } =
+    useQuery<UserApiResponse>({
+      queryKey: [
+        'table-data',
+        columnFilters, //refetch when columnFilters changes
+        globalFilter, //refetch when globalFilter changes
+        pagination.pageIndex, //refetch when pagination.pageIndex changes
+        pagination.pageSize, //refetch when pagination.pageSize changes
+        sorting, //refetch when sorting changes
+      ],
+      queryFn: async () => {
+        const fetchURL = new URL(
+          '/api/data',
+          process.env.NODE_ENV === 'production'
+            ? 'https://www.material-react-table.com'
+            : 'http://localhost:3000',
+        );
+        fetchURL.searchParams.set(
+          'start',
+          `${pagination.pageIndex * pagination.pageSize}`,
+        );
+        fetchURL.searchParams.set('size', `${pagination.pageSize}`);
+        fetchURL.searchParams.set(
+          'filters',
+          JSON.stringify(columnFilters ?? []),
+        );
+        fetchURL.searchParams.set('globalFilter', globalFilter ?? '');
+        fetchURL.searchParams.set('sorting', JSON.stringify(sorting ?? []));
+
+        const response = await fetch(fetchURL.href);
+        const json = (await response.json()) as UserApiResponse;
+        return json;
+      },
+      keepPreviousData: true,
+    });
+
+  const columns = useMemo<MRT_ColumnDef<User>[]>(
+    () => [
+      {
+        accessorKey: 'firstName',
+        header: 'First Name',
+      },
+      {
+        accessorKey: 'lastName',
+        header: 'Last Name',
+      },
+      {
+        accessorKey: 'address',
+        header: 'Address',
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+      },
+      {
+        accessorKey: 'phoneNumber',
+        header: 'Phone Number',
+      },
+    ],
+    [],
+  );
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data?.data ?? []} //data is undefined on first render
+      initialState={{ showColumnFilters: true }}
+      manualFiltering
+      manualPagination
+      manualSorting
+      muiToolbarAlertBannerProps={
+        isError
+          ? {
+            color: 'error',
+            children: 'Error loading data',
+          }
+          : undefined
+      }
+      onColumnFiltersChange={setColumnFilters}
+      onGlobalFilterChange={setGlobalFilter}
+      onPaginationChange={setPagination}
+      onSortingChange={setSorting}
+      renderTopToolbarCustomActions={() => (
+        <Tooltip arrow title="Refresh Data">
+          <IconButton onClick={() => refetch()}>
+            <RefreshIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+      rowCount={data?.meta?.totalRowCount ?? 0}
+      state={{
+        columnFilters,
+        globalFilter,
+        isLoading,
+        pagination,
+        showAlertBanner: isError,
+        showProgressBars: isFetching,
+        sorting,
+      }}
+    />
+  );
+};
+
+const queryClient = new QueryClient();
+
+const ExampleWithReactQueryProvider = () => (
+  <QueryClientProvider client={queryClient}>
+    <Example />
+  </QueryClientProvider>
+);
+
+export default ExampleWithReactQueryProvider;
